1cf27992951ada9ae811481f9faa52bdac56e703
Vincent Mancheron <vmancheron@yahoo.fr>
2021-07-05 16:54:47 +0200

    05/07/21 16:54

--- header.php
+++ header.php
@@ -5,8 +5,9 @@ html5up.net | @ajlkn
 Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 -->
 <html <?php language_attributes(); ?>>
+<head>
     <title>Future Imperfect by HTML5 UP</title>
-    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
     <?php wp_head(); ?>
 </head>
